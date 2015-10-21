14d5485f88d7ef4d8a8f01657ea2fdf269eeeb84
suya xu <tataxusuya@hotmail.com>
2015-10-21 05:34:11 -0700

    filter function for data added

--- js/script.js
+++ js/script.js
@@ -45,23 +45,36 @@ function loadData() {
     nytElem.innerHTML = ""; // Clear out old data before new request
     var nytimesAPI = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='
                      + city + '&sort=newest&api-key=1ac10106955053261890691c92ae6847:12:72971373';
+    // Higher order function betterData to filter raw data from NYtimes
+    function betterData (items, filter) {
+        var passed = [];
+        items.forEach(function(item){
+            if (filter(item)) {
+              passed.push(item);
+            }
+        });
+        return passed;
+    }
     // Use JQuery to request JSON file 
     $.getJSON(nytimesAPI, function(data) {
         var items = data.response.docs;
         var text, title, news;
         var allNews = '';
         nytHeaderElem.innerHTML = 'New York Times Article About ' + city.toUpperCase(); // Make city data to uppercase
-        // For all the data get from Newyork Times, based on each item in the object, modify DOM accordingly
-        items.forEach(function(item) {
-            // Clean up raw data from NY Times,
-            if (item["headline"]["main"].length <= 100){
-                // Organize data for title and text for each news accordingly
-                title = '<a href="' + item["web_url"] + '">' + item["headline"]["main"].toUpperCase() + "</a>";
-                text = '<p>' + item["pub_date"].slice(0,10) + '<br>' + item["snippet"] + '</p>';
-                news = '<li class="article">' + title + text + '</li>';
-            };
-            // concat all the organized data together
-            allNews += news;
+
+        // Call betterData pass in anonymous function to set specific requirments this dataset 
+        var nyPure = betterData(items, function(item){
+            return item["headline"]["main"].length > 0 && item["headline"]["main"].length <= 100 
+                && item["snippet"].length >0 && item["snippet"].length <= 500;
+        });
+        // Based on puerer data modify DOM accordingly
+        nyPure.forEach(function(item) {
+        // Organize data for title and text for each news accordingly
+            title = '<a href="' + item["web_url"] + '">' + item["headline"]["main"].toUpperCase() + "</a>";
+            text = '<p>' + item["pub_date"].slice(0,10) + '<br>' + item["snippet"] + '</p>';
+            news = '<li class="article">' + title + text + '</li>';
+        // concat all the organized data together
+        allNews += news;
         });
         // replace ny container with new formatted data
         nytElem.innerHTML = allNews;
@@ -86,13 +99,29 @@ function loadData() {
     });
     // On success oragnize data and update DOM accordingly
     function successCallback(items) {
+        var wikiAllRaw = [];
+        // Reorganize data
+        var Page = function(name, url, brief){
+            this.name = name;
+            this.url = url;
+            this.brief = brief; 
+        };
+
+        items[1].forEach(function(item, index){
+           var wikiRaw = new Page(item, items[3][index], items[2][index]);
+           wikiAllRaw.push(wikiRaw);
+        });
+
+        // Call betterData pass in anonymous function to set specific requirments this dataset 
+        var wikiPure = betterData(wikiAllRaw, function(item){
+            return item.name.length > 0 && item.url.length > 0 && item.brief.length > 0;
+        });
+
         var links = '';
-        items[1].forEach(function(item, index){ 
-            if (items[2][index].length !==0){
-                var wikiLink = '<p><br><a href ="' + items[3][index] + '">' + item + "</a></p>";
-                var wikiBrief = '<p>' + items[2][index] + '</p>';
-                links += wikiLink + wikiBrief;
-            }
+        wikiPure.forEach(function(item, index){ 
+            var wikiLink = '<p><br><a href ="' + item.url + '">' + item.name.toUpperCase() + "</a></p>";
+            var wikiBrief = '<p>' + item.brief + '</p>';
+            links += wikiLink + wikiBrief;
         });
         wikiElem.innerHTML = links;
         clearTimeout(wikiRequestTimeout); // Since success, timeout function no longer needed
