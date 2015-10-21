94e6b231ce02cc1d16f88b18c3b8758518b03daa
suya xu <tataxusuya@hotmail.com>
2015-10-20 18:08:29 -0700

    all API have callback on error

--- js/script.js
+++ js/script.js
@@ -4,32 +4,45 @@
 "use strict";
 
 
-//loadData
+//invoke loadData after submit form
 document.getElementById("form-container").onsubmit = function() {
     loadData();
     return false;
 };
-
-function loadData() {
-    var wikiElem = document.getElementById('wikipedia-links');
-    var wikiHeader = document.getElementById('wikipedia-header');
-    var nytHeaderElem = document.getElementById('nytimes-header');
-    var nytElem = document.getElementById('nytimes-articles');
+// modify DOM
+function loadData() {   
+    var greetingMsg = document.getElementById('greeting');
+    greetingMsg.innerHTML = "";//clear out old data before new request
     var city = document.getElementById('city').value;
     var street = document.getElementById('street').value;
-    var address = city+','+street;
-    var source ='https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address;
-    var nytimesAPI ='http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+city+'&sort=newest&api-key=1ac10106955053261890691c92ae6847:12:72971373';
-    var backgroungImg= document.getElementsByTagName('img')[0];
-    
-    // clear out old data before new request
-    wikiElem.innerHTML = "";
-    nytElem.innerHTML = "" ;
+    var address;
+
+    //update greeting message
+    function getUserInput(street, city, callback) {
+        address = street + " , " + city;
+        // Make sure the callback is a function​
+        if (typeof callback === "function") {
+            callback(address);
+        }
+    }
+
+    function greetUser(address) {
+        greetingMsg.innerHTML =  '<h2 class="greeting">Welcome to '+ address + '</h2>';
+    }
+
+    getUserInput(street, city, greetUser);
+
 
     // google streetview API
+    var backgroungImg= document.getElementsByTagName('img')[0];
+    var source ='https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address;
     backgroungImg.src = source; 
 
     // newyork time API use getJSON
+    var nytHeaderElem = document.getElementById('nytimes-header');
+    var nytElem = document.getElementById('nytimes-articles');
+    nytElem.innerHTML = "";
+    var nytimesAPI ='http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+city+'&sort=newest&api-key=1ac10106955053261890691c92ae6847:12:72971373';
     $.getJSON(nytimesAPI, function(data) {
         var items = data.response.docs.slice(5);
         var text, title, news;
@@ -37,7 +50,7 @@ function loadData() {
         nytHeaderElem.innerHTML='New York Times Article Abourt '+city.toUpperCase();
         items.forEach(function(item) {
             console.log(item);
-            if (item["headline"]["main"].length<=100){
+            if (item["headline"]["main"].length <= 100){
                 title ='<a href="' + item["web_url"] + '">' + item["headline"]["main"].toUpperCase()+"</a>";
                 text = '<p>' + item["pub_date"].slice(0,10) + '<br>' + item["snippet"] + '</p>';
                 news = '<li class="article">'+ title + text +'</li>';
@@ -46,10 +59,12 @@ function loadData() {
         });
         nytElem.innerHTML= allNews;
     }).error(function(){
-        nytHeaderElem.text('New York Times Cannot Be Load');
+        nytHeaderElem.innerHTML='New York Times Cannot Be Load';
     });
     //wiki API use Ajax
-
+    var wikiElem = document.getElementById('wikipedia-links');
+    var wikiHeader = document.getElementById('wikipedia-header');
+    wikiElem.innerHTML = "";
     var wikiRequestTimeout = setTimeout(function(){
         wikiHeader.innerHTML='Wikipedia Cannot Be Load';
     },3000);
@@ -59,7 +74,7 @@ function loadData() {
         items[1].forEach(function(item, index){ 
             var wikilink = '<p><br><a href ="' +items[3][index]+'">' + item +"</a></p>";
             links+=wikilink;
-            });
+        });
         wikiElem.innerHTML=links;
         clearTimeout(wikiRequestTimeout);
     }
